6e6199c8a1b272e8f5f6a00fae22ad49bd414f85
Priya Sonkar <priyasonkar0003@gmail.com>
2024-03-23 14:27:27 +0530

    second Commit

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { services } from '../assets/data/services'
+import ServiceCard from '../components/services/ServiceCard'
+
 
 const Services = () => {
   return (
-    <div>
-      Services
-    </div>
+    <section>
+      <div className="container">
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-[30px] '>
+          {services.map((item, index)=> 
+          <ServiceCard item ={item} index={index} key={index} />)}
+          
+        </div>
+      </div>
+    </section>
   )
+    
 }
 
 export default Services
