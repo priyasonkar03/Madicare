b70afea9b0c9cd34556a507c8d92d6f5e2b41988
Priya Sonkar <priyasonkar0003@gmail.com>
2024-04-03 09:41:19 +0530

    thirdcommit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,57 @@
+import User from '../models/UserSchema.js'
+import Doctor from  '../models/DoctorSchema.js'
+import jwt from  'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+export const register = async(req, res)=>{
+    const {email, password, name, photo, gender} = req.body;
+    try {
+        let user =  null
+        if (role === 'patient'){
+            user = await User.findOne({email})
+        }else if(role === 'doctor'){
+            user = Doctor.findOne({email})
+        }
+        //check if user exist
+        if (user){
+            return res.status(400).json({message: 'User alreadu exist'})
+        }
+        //hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
+        if (role === 'patient'){
+            user = new User({
+                name,
+                email, 
+                password: hashPassword,
+                photo,
+                gender,
+                role
+            })
+        }
+        if (role === 'doctor'){
+            user = new Doctor({
+                name,
+                email, 
+                password: hashPassword,
+                photo,
+                gender,
+                role
+            })
+        }
+
+        await user.save()
+
+        res.status(200).json({success:true , message:'User successfully created'})
+
+    } catch (error) {
+        res.status(200).json({success:true , message:'Internal server error, try again'})
+    }
+}
+
+export const login = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
